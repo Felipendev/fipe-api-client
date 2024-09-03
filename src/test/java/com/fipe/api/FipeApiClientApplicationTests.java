9c6d03f4ec654f23a541846b794ec60f77c2027c
Felipe Pedroso <felipepedroso@sis-it.com>
2024-09-03 13:32:33 -0300

    first commit

---
+++ src/test/java/com/fipe/api/FipeApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fipe.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
