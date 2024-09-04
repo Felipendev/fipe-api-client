b28e898ded1f5c628a35b76b758cb70d7607908a
Felipe Pedroso <felipepedroso@sis-it.com>
2024-09-04 08:35:38 -0300

    feat: implements FipeFeignClient API

--- src/main/java/com/fipe/api/FipeApiClientApplication.java
+++ src/main/java/com/fipe/api/FipeApiClientApplication.java
@@ -1,8 +1,13 @@
 package com.fipe.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "Fipe table API", version = "1.0", description = "API to query vehicle values from the FIPE Table"))
 @SpringBootApplication
 public class FipeApiClientApplication {
 
