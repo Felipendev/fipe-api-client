b1b71b1082e7fe48ebded310108f57b371ad974f
Felipe Pedroso <felipepedroso@sis-it.com>
2024-10-11 08:13:00 -0300

    refactor: update FipeApplicationService to use new mappers for brand, model, and year

--- src/main/java/com/fipe/api/application/service/FipeService.java
+++ src/main/java/com/fipe/api/application/service/FipeService.java
@@ -1,15 +1,15 @@
 package com.fipe.api.application.service;
 
-import com.fipe.api.application.api.response.BrandResponse;
-import com.fipe.api.output.ModelResponse;
-import com.fipe.api.application.api.response.YearResponse;
+import com.fipe.api.output.BrandOutput;
+import com.fipe.api.output.ModelOutput;
+import com.fipe.api.output.YearOutput;
 import com.fipe.api.output.ValueOutput;
 
 import java.util.List;
 
 public interface FipeService {
-    List<BrandResponse> getVehicleBrands(String vehicle);
-    ModelResponse getModelsByBrand(String vehicleType, String brandId);
-    List<YearResponse> getYearsByModel(String vehicleType, String brandId, String modelId);
+    List<BrandOutput> getVehicleBrands(String vehicle);
+    ModelOutput getModelsByBrand(String vehicleType, String brandId);
+    List<YearOutput> getYearsByModel(String vehicleType, String brandId, String modelId);
     ValueOutput getVehicleDetail(String vehicleType, String brandId, String modelId, String yearId);
 }
